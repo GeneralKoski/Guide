d793a133a5492764aab763d348d9148a6d870854
GeneralKoski <mtrajkovski1@outlook.com>
2024-09-25 11:45:13 +0200

    aggiunta cartella Javascript e Typescript

---
+++ JavaScript_TypeScript/es6/main.js
@@ -0,0 +1,5 @@
+import {a, add, divide} from './utility.js'
+
+console.log(a)
+console.log(add(1, 2));
+console.log(divide(4,2));
---
+++ JavaScript_TypeScript/es6/utility.js
@@ -0,0 +1,9 @@
+export const a = 1;
+
+export const add = (a, b) => {
+    return a + b;
+};
+
+export const divide = (a, b) => {
+    return a/b;
+};
