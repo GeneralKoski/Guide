1d96352df24c2871d672c0fe819273c47ffc4bac
GeneralKoski <mtrajkovski1@outlook.com>
2024-11-12 17:32:17 +0100

    feat: __callStatic magic method

---
+++ PHP/MAGIC_METHODS/__callStatic.php
@@ -0,0 +1,25 @@
+<?php
+// Se nell'URL della pagina aggiungi ?name=nome&age=123 ecc. nella REQUEST, prende i valori passati li
+    
+class Request {
+    protected $data;
+    public function __construct() {
+        echo "Passando dal costruttore\n";
+        if(!empty($_REQUEST) && empty(self::$data)) {
+            $this->data = $_REQUEST;
+        }
+    }
+    public function getData() {
+        return $this->data;
+    }
+    public static function __callStatic($name, $args) {
+        var_dump(func_get_args());
+        $obj = new static();
+        switch ($name) {
+            case 'all':
+                return $obj->getData();
+        }
+    }    
+}
+
+var_dump(Request::all('name'));
