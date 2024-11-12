31bcd80c77283ac291ddb3647e36e3a418b423bd
GeneralKoski <mtrajkovski1@outlook.com>
2024-11-12 16:01:49 +0100

    feat: Tipificazione

--- PHP/CLASSI/Tipificazione.php
+++ PHP/CLASSI/Tipificazione.php
@@ -1,9 +1,17 @@
 <?php
 // Ad ogni varabile dichiaro il suo tipo (string, int, bool ecc.)
 class Person {
-    public string $name;
-    public string $lastName;
-    public int $age = 0;
-    public ?string $address;
-    public ?string $phone;
+    readonly public string $name;
+    function __construct(string $name, public string $lastName, public int $age, public ?string $address, public ?string $phone) {
+        $this->name = $name;
+        $this->lastName = $lastName;
+        $this->age = $age;
+        $this->address = $address;
+        $this->phone = $phone;
+    }
 }
+
+$person = new Person("Martin", "Trajkovski", "22", "Via Lino Vescovi 38", "3755007982");
+var_dump($person);
+
+// $person->name = "giovanni"; // Non si può modificare dopo l'inizializzazione in quanto è readonly
