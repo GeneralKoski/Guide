665f281d459b614dc524a7afc1ff336436121d5f
GeneralKoski <mtrajkovski1@outlook.com>
2024-11-12 14:09:33 +0100

    feat: Classi astratte

---
+++ PHP/CLASSI/abstract.php
@@ -0,0 +1,20 @@
+<?php
+abstract class Car {
+    protected $speed = 0;
+    abstract protected function speedUp(int $v);
+    public function getSpeed() {
+        return $this->speed;
+    }
+
+}
+class myCar extends Car {
+    protected $speed = 0;
+    public function speedUp(int $v)  {
+        $this->speed += $v;
+    }
+}
+
+$car = new myCar();
+$car->speedUp(30);
+var_dump($car);
+echo $car->getSpeed(); // Posso richiamarla dalla classe astratta
