314ca7e5b9e5b19043f170e079078299b0a06724
GeneralKoski <mtrajkovski1@outlook.com>
2024-11-12 15:21:03 +0100

    feat: Tipificazione.php

--- PHP/CLASSI/Tipificazione.php
+++ PHP/CLASSI/Tipificazione.php
@@ -1,2 +1,9 @@
 <?php
-echo "ciao";
+// Ad ogni varabile dichiaro il suo tipo (string, int, bool ecc.)
+class Person {
+    public string $name;
+    public string $lastName;
+    public int $age = 0;
+    public ?string $address;
+    public ?string $phone;
+}
