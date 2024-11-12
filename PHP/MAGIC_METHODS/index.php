6916e11aa1f5de8f3399db0106ab5f053ad6c40a
GeneralKoski <mtrajkovski1@outlook.com>
2024-11-12 17:16:03 +0100

    feat: Aggiunta magic methods

---
+++ PHP/MAGIC_METHODS/index.php
@@ -0,0 +1,22 @@
+<?php
+// Se nell'URL della pagina aggiungi ?name=nome nella REQUEST prende i valori passati li
+    
+class Request {
+    protected $data;
+    public function __construct() {
+        echo "Passando dal costruttore\n";
+        if(!empty($_REQUEST) && empty(self::$data)) {
+            $this->data = $_REQUEST;
+        }
+    }
+    public function getData() {
+        return $this->data;
+    }
+    public function __get($name) {
+        echo "Passando da __get\n";
+        return isset($this->data[$name]) ? $this->data[$name] : null;
+    }    
+}
+
+$obj = new Request();
+var_dump($obj->getData());
