95b05f7232dacdd167a6f2d0bfa937c76876e04d
GeneralKoski <mtrajkovski1@outlook.com>
2024-11-12 17:40:46 +0100

    feat: __call magic method

---
+++ PHP/MAGIC_METHODS/__call.php
@@ -0,0 +1,24 @@
+<?php
+// Se nell'URL della pagina aggiungi ?name=nome nella REQUEST prende i valori passati li
+    
+class Request {
+    protected $data;
+    public function __construct() {
+        echo "Passando dal costruttore\n";
+        if(!empty($_REQUEST) && empty(self::$data)) {
+            $this->data = $_REQUEST;
+        }
+    }
+    public function getData() {
+        return $this->data;
+    }
+    public function __call($name, $args) {
+        $var = strtolower(str_replace('get', '', $name));
+        return isset($this->data[$var]) ? $this->data[$var] : null;
+    }
+    
+}
+
+$obj = new Request();
+var_dump($obj->getName());
+var_dump($obj->getAge());
