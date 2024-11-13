a9a54b05166436f8d253efc68a5975e51edde975
GeneralKoski <mtrajkovski1@outlook.com>
2024-11-13 13:01:03 +0100

    feat: Metodo __call

--- PHP/MAGIC_METHODS/__call.php
+++ PHP/MAGIC_METHODS/__call.php
@@ -1,18 +1,22 @@
 <?php
 // Se nell'URL della pagina aggiungi ?name=nome nella REQUEST prende i valori passati li
-    
-class Request {
+
+class Request
+{
     protected $data;
-    public function __construct() {
+    public function __construct()
+    {
         echo "Passando dal costruttore\n";
-        if(!empty($_REQUEST) && empty(self::$data)) {
+        if (!empty($_REQUEST) && empty(self::$data)) {
             $this->data = $_REQUEST;
         }
     }
-    public function getData() {
+    public function getData()
+    {
         return $this->data;
     }
-    public function __call($name, $args) {
+    public function __call($name, $args)
+    {
         $var = strtolower(str_replace('get', '', $name));
         return isset($this->data[$var]) ? $this->data[$var] : null;
     }
